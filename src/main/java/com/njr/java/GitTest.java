de93db8cefae3892085b5d2276e68dea7737cbb9
Layne <Layne@niujiangren.com>
2022-08-05 18:34:52 +0800

    second commit

--- src/main/java/com/njr/java/GitTest.java
+++ src/main/java/com/njr/java/GitTest.java
@@ -3,5 +3,6 @@ package com.njr.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world1111122222");
     }
 }
