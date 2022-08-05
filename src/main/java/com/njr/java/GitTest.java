f8a125d70ce6ac324804320388898b2e5664b5b1
Layne <Layne@niujiangren.com>
2022-08-05 18:35:38 +0800

    third commit

--- src/main/java/com/njr/java/GitTest.java
+++ src/main/java/com/njr/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world1111122222");
+        System.out.println("hello world11111222223333333");
     }
 }
